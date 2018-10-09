7e978f3ac76655c99a0d57a0d188a6f4cba1ffa0
Alan Fiscal <alanfiscalcampos@gmail.com>
2018-10-08 22:12:03 -0500

    Creacion de Historia por Sustitucion

--- scripts.js
+++ scripts.js
@@ -63,7 +63,7 @@ function regaño()
   }
 
 function game()
-{
+  {
   var tiro =Math.floor(Math.random()*3);
   console.log(tiro);
   var tiroJugador = Number(prompt("Elije piedra(0), Papel(1) o Tijera(2)"));
@@ -85,59 +85,60 @@ function game()
   }
 
 }
-function fibbo()
-{
-  let pri=0,seg=1,sig;
 
-  let x = Number(prompt("Cuantos numeros quieres ver de la serie?"));
-  if(x==0)
+function fibbo()
   {
-  console.log("Pa k me eligio");
-  }else if(x==1){
-    console.log("0 ");
-  }else if(x==2) {
-    console.log("0 1");
-  }else if(x==3)
-  {
-    console.log("0 1 ");
-  }else{
-    console.log(pri);
-    console.log(seg);
+    let pri=0,seg=1,sig;
+
+    let x = Number(prompt("Cuantos numeros quieres ver de la serie?"));
+    if(x==0)
+    {
+    console.log("Pa k me eligio");
+    }else if(x==1){
+      console.log("0 ");
+    }else if(x==2) {
+      console.log("0 1");
+    }else if(x==3)
+    {
+      console.log("0 1 ");
+    }else{
+      console.log(pri);
+      console.log(seg);
+    }
+    for(i=0;i<=x-3;i++)
+    {
+      sig=pri+seg;
+      pri=seg;
+      seg=sig;
+      console.log(sig);
+    }
   }
-  for(i=0;i<=x-3;i++)
-  {
-    sig=pri+seg;
-    pri=seg;
-    seg=sig;
-    console.log(sig);
-  }
-}
 
 function password()
-{
-  let pass=prompt("Cual es su contraseña?");
-  console.log(pass);
-
-  pass = pass.split("");
-
-  for(i=0;i<pass.length;i++)
   {
-    if(pass[i]=="a")
+    let pass=prompt("Cual es su contraseña?");
+    console.log(pass);
+
+    pass = pass.split("");
+
+    for(i=0;i<pass.length;i++)
     {
-      pass[i]=4;
-    }else if(pass[i]=="e")
-    {
-      pass[i]=3;
-    }else if(pass[i]=="i")
-    {
-      pass[i]=1;
-    }else if(pass[i]=="o")
-    {
-    pass[i]=0;
-  }else if(pass[i]=="t")
-    {
-    pass[i]=7;
-    }
+      if(pass[i]=="a")
+      {
+        pass[i]=4;
+      }else if(pass[i]=="e")
+      {
+        pass[i]=3;
+      }else if(pass[i]=="i")
+      {
+        pass[i]=1;
+      }else if(pass[i]=="o")
+      {
+      pass[i]=0;
+    }else if(pass[i]=="t")
+      {
+      pass[i]=7;
+      }
   }
   console.log(pass);
   pass=pass.join("");
@@ -145,27 +146,64 @@ function password()
 }
 
 function palindromo()
-{
-  let frase=prompt("Escribe la frase a verificar")
-  let fraseSinEspacios=frase.replace(/ /g,'');
-  let fraseEnMinusculas=fraseSinEspacios.toLowerCase();
+  {
+    let frase=prompt("Escribe la frase a verificar")
+    let fraseSinEspacios=frase.replace(/ /g,'');
+    let fraseEnMinusculas=fraseSinEspacios.toLowerCase();
 
-  let fraseVolteada='';
-  for(let i=fraseEnMinusculas.length-1;i>=0;i--)
-  {
-    fraseVolteada=fraseVolteada +fraseEnMinusculas[i];
-  }if(fraseEnMinusculas==fraseVolteada)
-  {
-    console.log(frase);
-    console.log(fraseVolteada);
-    console.log(fraseEnMinusculas + " Sí es un palindromo");
-  }else {
+    let fraseVolteada='';
+    for(let i=fraseEnMinusculas.length-1;i>=0;i--)
     {
-      console.log(fraseEnMinusculas+ " No es un Palindormo");
+      fraseVolteada=fraseVolteada +fraseEnMinusculas[i];
+    }if(fraseEnMinusculas==fraseVolteada)
+    {
+      console.log(frase);
+      console.log(fraseVolteada);
+      console.log(fraseEnMinusculas + " Sí es un palindromo");
+    }else {
+      {
+        console.log(fraseEnMinusculas+ " No es un Palindormo");
+      }
     }
-  }
-}
 
+  }
+
+
+
+
+
+
+//Declarando Valores de Name
+const historyButton = document.querySelector("#history-button");
+const historyName = document.querySelector("#name");
+const historyBox = document.querySelector("#history-box");
+//Declarando Valores de age
+const historyAge= document.querySelector("#age");
+//Declarando Valores de place
+const historyPlace=document.querySelector('#history-form').opc;
+//dECLARANDO Valores de Personaje
+const historyCharacter= document.querySelector("#history-form").opc2;
+
+historyButton.addEventListener('click',generateHistory);
+
+function generateHistory()
+  {
+    event.preventDefault();//Sirve para que el formulario no se refreseque y se borre el contenido, pero SI guarda los datos
+    let name=historyName.value;
+    let age=historyAge.value;
+    let place=historyPlace.value;
+    let character=historyCharacter.value;
+    let history="Hola mi nombre es: " + name + " y tengo " + age + " años. Todo empezó en la bella ciudad de "+ place + ". Cuando de repente me encontre con un " + character ;
+
+    // if(opc2 == id="character1")
+    // {
+    //   character=
+    // }else if(opc2 == id="character2")
+    // {
+    //
+    // }
+    historyBox.innerHTML= history; //Inyectar un valor en HTML
+  }
 // convertArraytoObject()
 // {
 //   let x =prompt("Ingrese lo que quiere en el Array");
